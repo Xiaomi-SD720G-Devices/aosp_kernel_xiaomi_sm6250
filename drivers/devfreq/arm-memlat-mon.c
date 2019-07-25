1b251cb9cb79484632222ab0d4b6ff3547b80997
Minchan Kim <minchan@google.com>
2019-07-25 15:52:01 +0900

    memlat: don't read perf event if other CPU is under scm call
    memlat works based on perf event to calculate algorithm.
    Perf event needs IPI to read count from other CPU.
    However, if the CPU is under heavy scm call(+20ms), it could
    be stuck until the CPU could complete the scm call.
    It would cause long latency.
    
    This patch adds global atomic count to check whether scm is
    going. If it's under going, memlat doesn't invoke perf event
    function to avoid IPI stuck. Although there is still race to
    check it, the race should be small enough so effectivey, it
    mitigate such long latency.
    
    Please check to memlat owner what happens when read_event
    sometimes returns zero due to scm call in parallel.
    
    Bug: 134440633
    Change-Id: I0403d151ededcc80d531c8cd81daa783000902d1
    Signed-off-by: Minchan Kim <minchan@google.com>
    [lazerl0rd: Cleaned up the patch slightly.]
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: reaPeR1010 <reaPeR10x10x@gmail.com>
    Signed-off-by: Abhishek001konni <Abhishek001konni@gmail.com>

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -149,6 +149,13 @@ static void delete_events(struct cpu_pmu_stats *cpustats)
 {
 	int i;
 
+	/*
+	 * Some of SCM call is very heavy(+20ms) so perf IPI could
+	 * be stuck on the CPU which contributes long latency.
+	 */
+	if (under_scm_call())
+		return;
+
 	for (i = 0; i < ARRAY_SIZE(cpustats->events); i++) {
 		cpustats->events[i].prev_count = 0;
 		if (cpustats->events[i].pevent) {
